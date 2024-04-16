d94891c30cf5eceebec9a05280af67a717b08665
Ge0frey <jeffkimzyjoe@gmail.com>
2024-04-16 17:12:57 +0300

    A db instance

---
+++ Users.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        id: 1,
+        name: 'Geofrey',
+        email: 'geofrey@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'kimani',
+        email: 'kimani@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'jeff',
+        email: 'jeff@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'recen',
+        email: 'recen@gmail.com'
+    }
+];
+
+module.exports = users;
