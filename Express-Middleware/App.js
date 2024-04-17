f43891591466fcf54492cc9656b098fd087c01ed
Ge0frey <jeffkimzyjoe@gmail.com>
2024-04-17 18:01:19 +0300

    Error handling in express js

--- Express-Middleware/App.js
+++ Express-Middleware/App.js
@@ -2,12 +2,10 @@ import express from 'express'
 
 const app = express();
 
-app.use (middleWare1);
-app.use(middleWare2)
-
 function middleWare1 (req, res, next) {
     console.log(`I am middleware 1`)
-    next()
+    const errObj = new Error (`I am an error`)
+    next(errObj)
 }
 
 function middleWare2 (req, res, next) {
@@ -15,14 +13,21 @@ function middleWare2 (req, res, next) {
     next()
 }
 
-function middleWare3 (req, res, next) {
-    console.log(`I am middleware 3`)
-    next()
+function errorHandler (err, req, res, next) {
+    const code = 404
+   if (err) {
+    res.send (`This is an error code: ${code}`)
+}
 }
 
-app.get ('/' ,middleWare3 , (req, res, next) => {
+app.use (middleWare1);
+app.use(middleWare2)
+
+app.get ('/' , (req, res, next) => {
     console.log(`I am the standard express function`)
     res.send(`This is an express App`)
 })
+
+app.use(errorHandler);
 
 app.listen (3000);
