79ee8a1d28ead477193bad8df8d1daea196fbafa
Ge0frey <jeffkimzyjoe@gmail.com>
2024-04-17 12:16:02 +0300

    Sends a get request to get users

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import userRoutes from './routes/api/Users.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send (`<h1>Hello world<h1>`)
-})
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use ('/api/Users', userRoutes)
 
 app.listen(3000, () => {
     console.log(`Server started on port 3000`)
